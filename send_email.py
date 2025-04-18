dc434d80d35d3e9c591f048d0833331a50c233d5
Avren-24 <202483890024@nuist.edu.cn>
2025-04-18 08:39:57 +0800

    Combined seccessfully

--- send_email.py
+++ send_email.py
@@ -2,20 +2,22 @@ import smtplib
 from email.message import EmailMessage
 
 from_email_addr = "1552038394@qq.com"
-from_email_pass = "375097ws"
-
+from_email_pass = "kayyffzchnoohade"
 to_email_addr = "1552038394@qq.com"
 
 msg = EmailMessage()
+
 body = "Hello from Raspberry Pi"
 msg.set_content(body)
 
-msg["Avren"] = from_email_addr
-msg['Avren'] = to_email_addr
+msg['From'] = from_email_addr
+msg['To'] = to_email_addr
 
 msg['Subject'] = 'TEST EMAIL'
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP('smtp.qq.com', 587)
+
+server.starttls()
 
 server.login(from_email_addr, from_email_pass)
 
